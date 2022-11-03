1e72cb0797d2fcd0665efc34c7708a66122b2d12
Santiago Lozano <santiago@MacBook-Pro-de-Santiago.local>
2022-11-03 18:44:15 -0500

    Added tests

--- src/app/plants/plants-list/plants-list.component.spec.ts
+++ src/app/plants/plants-list/plants-list.component.spec.ts
@@ -4,13 +4,19 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { PlantsListComponent } from './plants-list.component';
+import { Plant } from '../plant';
+import { faker } from '@faker-js/faker';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('PlantsListComponent', () => {
   let component: PlantsListComponent;
   let fixture: ComponentFixture<PlantsListComponent>;
+  let debug: DebugElement;
+  let expectedRowCount = 3;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
       declarations: [ PlantsListComponent ]
     })
     .compileComponents();
@@ -19,10 +25,31 @@ describe('PlantsListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantsListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < expectedRowCount; i++) {
+      const plant = new Plant(
+        faker.datatype.number(10),
+        faker.datatype.string(30),
+        faker.datatype.string(30),
+        faker.datatype.string(30),
+        faker.datatype.string(30),
+        faker.datatype.number(3),
+        faker.datatype.string(30),
+        );
+        component.plantsList.push(plant);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 4 rows', () => {
+    const headerRowCount = 1;
+    console.log(debug.queryAll(By.css("tr")).length.toString());
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(expectedRowCount+headerRowCount)
   });
 });
--- src/app/plants/plants.service.spec.ts
+++ src/app/plants/plants.service.spec.ts
@@ -2,10 +2,12 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { PlantsService } from './plants.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Plants', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule],
       providers: [PlantsService]
     });
   });
@@ -13,4 +15,5 @@ describe('Service: Plants', () => {
   it('should ...', inject([PlantsService], (service: PlantsService) => {
     expect(service).toBeTruthy();
   }));
+
 });
